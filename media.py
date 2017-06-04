9e1aa4df87301fe795eeda929163e980de62f8f8
Sai2sree <chaitunagarajugari@gmail.com>
2017-06-04 19:45:02 -0400

    Adding media.py, entertainment_center.py, and fresh_tomatoes.py

---
+++ media.py
@@ -0,0 +1,9 @@
+# Create a data structure (i.e. a Python Class) to store movies, including movie title, box art URL (or poster URL) and a YouTube link to the movie trailer
+class Movie():
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+    
+
+

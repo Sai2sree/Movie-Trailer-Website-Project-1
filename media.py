8853514aef2fd95667c4066c897f3b7d714cb126
SREE SAI CHAITANYA NAGARAJUGARI <chaitunagarajugari@gmail.com>
2017-06-05 18:31:16 -0400

    Formatted code to meet PEP8 requirements

--- media.py
+++ media.py
@@ -1,9 +1,9 @@
-# Create a data structure (i.e. a Python Class) to store movies, including movie title, box art URL (or poster URL) and a YouTube link to the movie trailer
+# Create a data structure to store movie details
+# Details include movie title, box art URL and a YouTube link to movie trailer
+
+
 class Movie():
     def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    
-
-
